b143b62f3da5a3065255cb8a015f45f5f05bc118
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-06 17:24:53 -0400

    created Follower component, rendering followers and their data on page

--- github-user-card-react/src/components/User.js
+++ github-user-card-react/src/components/User.js
@@ -1,17 +1,43 @@
+import axios from 'axios'
 import React from 'react'
+import Follower from './Follower'
 
 class User extends React.Component{
-    
+    constructor() {
+        super();
+        this.state = {
+            followers: []
+        }
+    }
+    componentDidMount() {
+        axios.get(this.props.userData.followers_url)
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                followers: res.data
+            })
+            console.log(this.state)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
         const { userData } = this.props 
         return (
             <div>
                 <h2>{userData.name}</h2>
+                <a href={userData.url}>{userData.url}</a>
                 <h4>{userData.login}</h4>
-                <h4>{userData.url}</h4>
-                <h4>{userData.followers}</h4>
-                <h4>{userData.following}</h4>
-                <h4>{userData.location}</h4>
+                <p>{userData.location}</p>
+                <p>{userData.followers}</p>
+                <p>{userData.following}</p>
+                <div> Followers
+                    {this.state.followers.map(follower => {
+                        return <Follower follower={follower} key={follower.id}/>
+                    })}
+                </div>
             </div>
         )
     }
